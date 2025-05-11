a44b62029f1aa4dee8c0d796de1c02d637b20809
scojr <180873402+scojr@users.noreply.github.com>
2025-05-11 13:48:41 -0500

    Add getValidMoves function

--- main.js
+++ main.js
@@ -2,19 +2,34 @@ class Knight {
   constructor(position, target) {
     this.position = position;
     this.target = target;
-    this.moves = getMoves(this.position);
+    this.moves = getValidMoves(this.position);
+  }
+  levelOrder(callback) {
+    levelOrderRecursion([])
   }
 }
 
-function getMoves(coordinates) {
+function getValidMoves(coordinates) {
   const x = coordinates[0];
   const y = coordinates[1];
-  return [[x - 1, y + 2],
-  [x + 1, y + 2],
-  [x + 2, y + 1],
-  [x + 2, y - 1],
-  [x + 1, y - 2],
-  [x - 1, y - 2],
-  [x - 2, y - 1],
-  [x - 2, y + 1]];
+  const validMoves = [];
+  const potentialMoves =
+    [[x - 1, y + 2],
+    [x + 2, y + 1],
+    [x + 1, y + 2],
+    [x + 2, y - 1],
+    [x + 1, y - 2],
+    [x - 1, y - 2],
+    [x - 2, y - 1],
+    [x - 2, y + 1]];
+  potentialMoves.forEach(move => {
+    if (move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7) validMoves.push(move);
+  })
+  return validMoves;
 }
+
+function knightMoves(start, end) {
+  const root = new Knight(start, end);
+}
+
+console.log(getValidMoves([0, 0]))
